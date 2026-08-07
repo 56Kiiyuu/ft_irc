8593d9ea58e9fdd5a81cf12f118b4aad7653315c
gchalme <gab.cht@icloud.com>
2026-08-07 16:03:27 +0200

    add err

--- srcs/commands/Kick.cpp
+++ srcs/commands/Kick.cpp
@@ -6,12 +6,16 @@
 #include "NumericReplies.hpp"
 
 void cmdKick(Server& server, Client::ClientInfo& sender, int socketFd, const Message& msg)
-{
-
+{	
+	if (msg.getParams().size() != 2)
+	{
+		std::string err = ERR_NEEDMOREPARAMS(sender.nickname, "KICK");
+		send(socketFd, err.c_str(), err.length(), 0);
+		return ;
+	}
 
 	std::string channel = msg.getParams()[0];
 	std::string target = msg.getParams()[1];
-
 
 	int indexChannel = -1;
 	for (std::size_t i=0 ; i < server.getChannels().size() ; i++)
@@ -30,8 +34,37 @@ void cmdKick(Server& server, Client::ClientInfo& sender, int socketFd, const Mes
 		return ;
 	}
 
-
 	Channels& chan = server.getChannels()[indexChannel];
+
+	// check que le user est dans le canal
+	bool isMember = chan.isOp(socketFd);
+	if (!isMember)
+	{
+		std::vector<int> users = chan.getUser();
+		for (std::size_t i = 0; i < users.size(); i++)
+		{
+			if (users[i] == socketFd)
+			{
+				isMember = true;
+				break;
+			}
+		}
+	}
+	if (!isMember)
+	{
+		send(socketFd, ERR_NOTONCHANNEL(sender.nickname, chan.getName()).c_str(), ERR_NOTONCHANNEL(sender.nickname, chan.getName()).length(), 0);
+		return;
+	}
+
+
+	// check que 'l'utilisateur est op
+	if (!chan.isOp(sender.fd))
+	{
+		std::string err = ERR_CHANOPRIVSNEEDED(sender.nickname, channel);
+		send(socketFd, err.c_str(), err.length(), 0);
+		return;
+	}
+
 
 	int userIndex = -1;
 	int ownerIndex = -1;
@@ -59,8 +92,9 @@ void cmdKick(Server& server, Client::ClientInfo& sender, int socketFd, const Mes
 
 	if (ownerIndex == -1 && userIndex == -1)
 	{
-		// ERR
-		return ;
+		std::string err = ERR_USERNOTINCHANNEL(sender.nickname, channel);
+		send(socketFd, err.c_str(), err.length(), 0);
+		return;
 	}
 
 	std::string prefix = ":" + sender.nickname + "!" + sender.user + "@127.0.0.1";
