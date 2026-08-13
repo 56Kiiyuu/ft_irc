5c58e18e1e304e37034d29b21d9e5231f029f6ed
kevlim <kevlim@f6r8s6.paris.42.school>
2026-08-13 15:23:12 +0200

    [FIXED] PING cmd

--- srcs/commands/Ping.cpp
+++ srcs/commands/Ping.cpp
@@ -4,14 +4,18 @@
 #include "Message.hpp"
 #include <iostream>
 
-void	cmdPing(Server& server, Client::ClientInfo& sender, int socketFd, const Message& msg)
+void cmdPing(Server& server, Client::ClientInfo& sender, int socketFd, const Message& msg)
 {
-	(void)server;
-	(void)sender;
-
-	if (!msg.getParams().empty())
+	(void) server;
+	(void) sender;
+	if (msg.getParams().empty())
 	{
-		std::string reply = ":server PONG server " + msg.getParams()[0] + "\r\n";
+		std::string reply = "PONG :ircserv\r\n";
 		send(socketFd, reply.c_str(), reply.length(), 0);
+		return;
 	}
+
+	std::string param = msg.getParams()[0];
+	std::string reply = ":ircserv PONG " + (param[0] == ':' ? param : ":" + param) + "\r\n";
+	send(socketFd, reply.c_str(), reply.length(), 0);
 }
