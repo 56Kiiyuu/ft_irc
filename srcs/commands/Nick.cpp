bd1f2760928b5adad373e0ca2dfb6714fe21d975
kevlim <kevlim@f4r6s5.paris.42.school>
2026-08-14 13:22:02 +0200

    [FIXED] Nick msg for everyone

--- srcs/commands/Nick.cpp
+++ srcs/commands/Nick.cpp
@@ -1,6 +1,7 @@
 #include "Command.hpp"
 #include "Server.hpp"
 #include "Client.hpp"
+#include "Channels.hpp"
 #include "Message.hpp"
 #include "NumericReplies.hpp"
 #include <iostream>
@@ -10,110 +11,121 @@
 
 static bool isValidNickname(const std::string& nick)
 {
-    // 9 chars maximum
-    if (nick.empty() || nick.length() > 9)
-        return false;
+	// 9 chars maximum
+	if (nick.empty() || nick.length() > 9)
+		return false;
 
-    // allowed to start
-    std::string allowedFirst = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ[]\\`^{}|_";
-    if (allowedFirst.find(nick[0]) == std::string::npos)
-        return false;
+	// allowed to start
+	std::string allowedFirst = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ[]\\`^{}|_";
+	if (allowedFirst.find(nick[0]) == std::string::npos)
+		return false;
 
-    std::string allowedChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789[]\\`^{}|_-";
-    for (size_t i = 0; i < nick.length(); ++i)
-    {
-        if (allowedChars.find(nick[i]) == std::string::npos)
-            return false;
-    }
-    return true;
+	std::string allowedChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789[]\\`^{}|_-";
+	for (size_t i = 0; i < nick.length(); ++i)
+	{
+		if (allowedChars.find(nick[i]) == std::string::npos)
+			return false;
+	}
+	return true;
 }
 
 // set le nickname
 void cmdNick(Server& server, Client::ClientInfo& sender, int socketFd, const Message& msg)
 {
-    std::string clientNick = sender.nickname.empty() ? "*" : sender.nickname;
+	std::string clientNick = sender.nickname.empty() ? "*" : sender.nickname;
 
-    if (!sender.hasPass)
-    {
-        std::string err = ERR_NOTREGISTERED(clientNick);
-        send(socketFd, err.c_str(), err.length(), 0);
-        return;
-    }
+	if (!sender.hasPass)
+	{
+		std::string err = ERR_NOTREGISTERED(clientNick);
+		send(socketFd, err.c_str(), err.length(), 0);
+		return;
+	}
 
-    // check if nickname
-    if (msg.getParams().empty() || msg.getParams()[0].empty())
-    {
-        std::string err = ERR_NONICKNAMEGIVEN(clientNick);
-        send(socketFd, err.c_str(), err.length(), 0);
-        std::cout << "[NICK] ERR_NONICKNAMEGIVEN (431) sent" << std::endl;
-        return;
-    }
+	// check if nickname
+	if (msg.getParams().empty() || msg.getParams()[0].empty())
+	{
+		std::string err = ERR_NONICKNAMEGIVEN(clientNick);
+		send(socketFd, err.c_str(), err.length(), 0);
+		std::cout << "[NICK] ERR_NONICKNAMEGIVEN (431) sent" << std::endl;
+		return;
+	}
 
-    std::string newNick = msg.getParams()[0];
+	std::string newNick = msg.getParams()[0];
 
-    // if invalid chars
-    if (!isValidNickname(newNick))
-    {
-        std::string err = ERR_ERRONEUSNICKNAME(clientNick, newNick);
-        send(socketFd, err.c_str(), err.length(), 0);
-        std::cout << "[NICK] ERR_ERRONEUSNICKNAME (432) pour : " << newNick << std::endl;
-        return;
-    }
+	// if invalid chars
+	if (!isValidNickname(newNick))
+	{
+		std::string err = ERR_ERRONEUSNICKNAME(clientNick, newNick);
+		send(socketFd, err.c_str(), err.length(), 0);
+		std::cout << "[NICK] ERR_ERRONEUSNICKNAME (432) pour : " << newNick << std::endl;
+		return;
+	}
 
-    // if nickname already used
-    if (server.isNickInUse(newNick))
-    {
-        std::string err = ERR_NICKNAMEINUSE(clientNick, newNick);
-        send(socketFd, err.c_str(), err.length(), 0);
-        std::cout << "[NICK] ERR_NICKNAMEINUSE (433) pour : " << newNick << std::endl;
-        return;
-    }
+	// if nickname already used
+	if (server.isNickInUse(newNick))
+	{
+		std::string err = ERR_NICKNAMEINUSE(clientNick, newNick);
+		send(socketFd, err.c_str(), err.length(), 0);
+		std::cout << "[NICK] ERR_NICKNAMEINUSE (433) pour : " << newNick << std::endl;
+		return;
+	}
 
-    std::string oldNick = sender.nickname;
-    sender.nickname = newNick;
-    std::cout << "[NICK] New nickname : " << sender.nickname << std::endl;
+	std::string oldNick = sender.nickname;
+	sender.nickname = newNick;
+	std::cout << "[NICK] New nickname : " << sender.nickname << std::endl;
 
-    // if already registered and changed nick
-    if (sender.isRegistered)
-    {
-        // nick msg format RFC (:OldNick!user@host NICK :NewNick)
-        std::string nickMsg = ":" + oldNick + "!" + sender.user + "@127.0.0.1 NICK :" + newNick + "\r\n";
-        
-        std::set<int> recipients;
-        recipients.insert(socketFd);
+	// if already registered and changed nick
+	if (sender.isRegistered)
+	{
+		// nick msg format RFC (:OldNick!user@host NICK :NewNick)
+		std::string nickMsg = ":" + oldNick + "!" + sender.user + "@127.0.0.1 NICK :" + newNick + "\r\n";
 
-        // announce other members in same channels
-        std::vector<Channels*>& channelList = server.getChannels();
-        for (size_t i = 0; i < channelList.size(); ++i)
-        {
-            if (channelList[i]->hasUser(socketFd))
-            {
-                const std::vector<int>& users = channelList[i]->getUsers();
-                for (size_t j = 0; j < users.size(); ++j)
-                {
-                    recipients.insert(users[j]); // std::set suppresses duplicates
-                }
-            }
-        }
+		std::set<int> recipients;
+		recipients.insert(socketFd);
 
-        for (std::set<int>::iterator it = recipients.begin(); it != recipients.end(); ++it)
-        {
-            send(*it, nickMsg.c_str(), nickMsg.length(), 0);
-        }
-        return;
-    }
+		// announce other members in same channels
+		std::vector<Channels>& channelList = server.getChannels();
+		for (size_t i = 0; i < channelList.size(); ++i)
+		{
+			std::vector<int>& users = channelList[i].getUser();
+			//member is in channel
+			bool isMember = false;
+			for (size_t j = 0; j < users.size(); ++j)
+			{
+				if (users[j] == socketFd)
+				{
+					isMember = true;
+					break;
+				}
+			}
+			// if in channel add all members of channel in the recipients list
+			if (isMember)
+			{
+				for (size_t j = 0; j < users.size(); ++j)
+				{
+					recipients.insert(users[j]); // std::set suppr dupes
+				}
+			}
+		}
+		//print
+		for (std::set<int>::iterator it = recipients.begin(); it != recipients.end(); ++it)
+		{
+			send(*it, nickMsg.c_str(), nickMsg.length(), 0);
+		}
+		return;
+	}
 
-    // if register with new nick
-    if (!sender.isRegistered && sender.hasPass && !sender.nickname.empty() && !sender.user.empty())
-    {
-        sender.isRegistered = true;
+	// if register with new nick
+	if (!sender.isRegistered && sender.hasPass && !sender.nickname.empty() && !sender.user.empty())
+	{
+		sender.isRegistered = true;
 
-        std::string welcome = RPL_WELCOME(sender.nickname, sender.user, "127.0.0.1");
-        send(socketFd, welcome.c_str(), welcome.length(), 0);
+		std::string welcome = RPL_WELCOME(sender.nickname, sender.user, "127.0.0.1");
+		send(socketFd, welcome.c_str(), welcome.length(), 0);
 
-        std::string yourHost = RPL_YOURHOST(sender.nickname);
-        send(socketFd, yourHost.c_str(), yourHost.length(), 0);
+		std::string yourHost = RPL_YOURHOST(sender.nickname);
+		send(socketFd, yourHost.c_str(), yourHost.length(), 0);
 
-        std::cout << "[SERVER] Client registered success (NICK) : " << sender.nickname << std::endl;
-    }
+		std::cout << "[SERVER] Client registered success (NICK) : " << sender.nickname << std::endl;
+	}
 }
